426268a8d61c44966a0348b6bb7614cf56b32d2f
trdmitr <trdmitr@gmail.com>
2023-12-24 18:59:41 +0400

    fin color

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 
 export const HomePage = () => {
-    const imgUrl = "https://sun9-22.userapi.com/impg/q8o1M4dWypUPThx3mGGocJl-ZuxbKiu5mvWoBA/5b8GW6P26Wo.jpg?size=596x482&quality=96&sign=754fe0391321581c9f808f5f929fc704&type=album"
+    const imgUrl = "https://sun9-45.userapi.com/impg/CIqfClbDC5izZxbz6TYaLmPd_Ag6g2_9fB_fqg/n6izRXxtT1A.jpg?size=733x730&quality=96&sign=5167fc50b0035710e75145b572af12cd&type=album"
     // const imgUrl2 = 'https://sun2-22.userapi.com/impg/2YiE2IW9r0friRMzAEniG_oR87ffPUfqKRswKw/vtMiX4nUf8o.jpg?size=300x300&quality=96&sign=7757c3bd97980caf5674faa89f7781c3&type=album'
     const imgAlt = "Каверы Подземки"
     
@@ -26,9 +26,9 @@ export const HomePage = () => {
                       <Link to={`/cavers${yearTrib23}`} className={classes.navLink23}>2023</Link> */}
                       <div className={classes.main}>
                       
-                          <div className={classes.article}><p >Трибьют к 70-летию БГ!</p></div>
+                          <div className={classes.article}><p >Кинчеву - 65!</p></div>
                           <div className={classes.article}><img fetchpriority="high" role="presentation"  src={imgUrl} width={100} alt={imgAlt} /></div>
-                          <p className={classes.nameAudio}>Медоманы. Время Луны</p>
+                          {/* <p className={classes.nameAudio}>Медоманы. Время Луны</p> */}
                           <audio controls 
           src={frontAudio} type="audio/mpeg" />
       
